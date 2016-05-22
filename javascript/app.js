7227627a4affa0055bfa9c5978e0efcd50784be3
Anthony Williams <pull.rebase>
2016-05-22 16:25:07 -0600

    added main.html

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,15 @@
-var app = angular.module("DevOps");
+var app = angular.module("DevOps",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+	when('/main',{
+		templateUrl: 'main.html',
+		controller: 'MainController'
+	});
+	
+}])
+
+.controller('MainController',[function(){
+	console.log('This is the MainController');
+
+}]);
